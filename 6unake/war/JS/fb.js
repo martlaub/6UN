49d88e1535c90dce4d49f3212f703b3792bb82d7
LiisR <liisreisberg86@gmail.com>
2014-03-25 09:17:41 +0200

    Kasutaja suunatakse ümber sisse logimisel, muidu retsepte jne ei näe

--- 6unake/war/JS/fb.js
+++ 6unake/war/JS/fb.js
@@ -1,3 +1,4 @@
+
 //load and initialize the SDK     
 window.fbAsyncInit = function() {
   FB.init({
@@ -10,35 +11,32 @@ window.fbAsyncInit = function() {
   });
   
   FB.Event.subscribe("auth.logout", function() {
-      window.top.location = 'http://6unapp.appspot.com/pealeht_enne.html'
+	 
+      window.top.location = 'http://6unapp.appspot.com/pealeht_enne.html';
+      
     });
   
   FB.Event.subscribe("auth.login", function() { 
-      window.top.location = 'http://6unapp.appspot.com/pealeht.html'
+	  window.top.location = 'http://6unapp.appspot.com/pealeht.html';
     });
    
    
-//Facebook Login
-  FB.Event.subscribe('auth.authResponseChange', function(response) { 
-	    if (response.status === 'connected') {
-	    	
-	      testAPI();
-	    } else if (response.status === 'not_authorized') {
-	      FB.login();
-	    } else {
-	      FB.login();
-	    }
+  FB.getLoginStatus(function(response) {
+	  if (response.status === 'connected') {
+		  
+	  } else if (response.status === 'not_authorized') {
+		  if(window.location!='http://6unapp.appspot.com/pealeht_enne.html'){
+				window.top.location = 'http://6unapp.appspot.com/pealeht_enne.html';
+				}
+	  } else {
+		  if(window.location!='http://6unapp.appspot.com/pealeht_enne.html'){
+				window.top.location = 'http://6unapp.appspot.com/pealeht_enne.html';
+				}
+	  }
 	  });
+  
 	  };
 
-	  (function(d, s, id) {
-		  var js, fjs = d.getElementsByTagName(s)[0];
-		  if (d.getElementById(id)) return;
-		  js = d.createElement(s); js.id = id;
-		  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=223636477840164";
-		  fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));
-	  
 	  // Load the SDK asynchronously		
 	  (function(d){
 	   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
@@ -48,25 +46,4 @@ window.fbAsyncInit = function() {
 	   ref.parentNode.insertBefore(js, ref);
 	  }(document));
 	  
-	  //sisse logimis funkt
-	  FB.login(function(response) {
-		    if (response.authResponse) {
-		        // The person logged into your app
-		    } else {
-		        // The person cancelled the login dialog
-		    }
-		});
-	  
-	//välja logimis funkt
-	  FB.logout(function(response) {
-	        // Person is now logged out
-	    });
-	  
-	  function testAPI() {
-	    console.log('Welcome!  Fetching your information.... ');
-	    FB.api('/me', function(response) {
-	      console.log('Good to see you, ' + response.name + '.');
-	    });
-	  }
-      
 	  
