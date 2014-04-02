03317c0be5fef902d474e0a8c52ffec5c6ce944c
LiisR <liisreisberg86@gmail.com>
2014-04-02 14:32:37 +0300

    Valideerimine, meldimine korras, uus facebooke nupp

--- 6unake/war/JS/fb.js
+++ 6unake/war/JS/fb.js
@@ -9,23 +9,12 @@ window.fbAsyncInit = function() {
     oauth      : true
   });
   
-  FB.Event.subscribe("auth.logout", function() {
-	  document.getElementById('peida').style.display = "none"; 
-	  if(window.location.pathname=='/retseptid.html' || window.location.pathname=='/_unake.html'){
-			window.location.href = 'pealeht.html';
-			} 
-    });
-  
-  FB.Event.subscribe("auth.login", function() { 
-	  document.getElementById('peida').style.display = "block";
-    });
-   
-  
   FB.getLoginStatus(function(response) {
 	  if (response.status === 'connected') {
+		  getUserInfo();
 		  document.getElementById('peida').style.display = "block";
-		  
-	  } else if (response.status === 'not_authorized') {
+
+	  } else if (response.status === 'not_authorized') {			 
 		  if(window.location.pathname=='/retseptid.html' || window.location.pathname=='/_unake.html'){
 			  window.location.href = 'pealeht.html';
 				} 
@@ -36,15 +25,42 @@ window.fbAsyncInit = function() {
 	  }
 	  });
   
-	  };
+  	    };
+		    
+//sisse logimine
+function Login(){
+	FB.login(function(response) {
+		if (response.authResponse){
+				    	getUserInfo();
+				    	document.getElementById('peida').style.display = "block";
+		  			} else {
+		  					}
+				 	},{scope: 'email,user_photos,user_videos'});
+			}
 
-	  // Load the SDK asynchronously		
-	  (function(d){
+//kasutaja info ja välja logimis nupp
+function getUserInfo() {
+    FB.api('/me', function(response) {   	
+		var str ="<img src='images/logout_FB.png' style='cursor:pointer; width: 10em;' onclick='Logout()'/></br>";
+			 document.getElementById("status").innerHTML=str; 	  	    
+		    							});
+		    }
+
+//välja logimis funkt.
+function Logout(){
+	FB.logout(function(){
+		document.location.reload();
+		if(window.location.pathname=='/retseptid.html' || window.location.pathname=='/_unake.html'){
+    		window.location.href = 'pealeht.html';
+				}
+						});
+			}
+
+// Load the SDK asynchronously
+(function(d){
 	   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
 	   if (d.getElementById(id)) {return;}
 	   js = d.createElement('script'); js.id = id; js.async = true;
 	   js.src = "//connect.facebook.net/en_US/all.js";
 	   ref.parentNode.insertBefore(js, ref);
 	  }(document));
-	  
-	  
